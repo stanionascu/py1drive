1fae708a5e739f3f31fe5706e5e73e65da3b181f
Stanislav Ionascu <stanislav.ionascu@gmail.com>
2015-07-01 23:29:53 +0200

    1drive.py: initial commit

---
+++ 1drive.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1drive.py -- cli frontend to onedrive
+# Copyright (C) 2015 Stanislav Ionascu
+
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+import sys, os
+
+"""OneDrive Python client"""
+
+config = []
+default_config_dir = os.path.expanduser('~/.py1drive')
+supported_actions = ['authorize',
+    'upload', 'download',
+    'list', 'info',
+    'mkdir', 'rm']
+config_file = None
+key_store_dir = None
+
+def build_arg_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-c',
+        '--config',
+        type=argparse.FileType('r'),
+        help='Config file to load')
+    parser.add_argument('-k',
+        '--key-store-dir',
+        type=str,
+        help='Location of the authorization key store dir')
+    parser.add_argument('action',
+        choices=supported_actions,
+        help='Action to execute on path')
+    parser.add_argument('local_path',
+        type=str,
+        nargs='?',
+        help='Path to local file/folder')
+    parser.add_argument('remote_path',
+        type=str,
+        help='Path to remote folder')
+    return parser
+
+def exec_action(action, local_path, remote_path):
+    from urllib.parse import urlparse
+    url = urlparse(remote_path)
+    sys.exit("Unsupported scheme: %s" % url.scheme)
+
+def save_config():
+    open(config_file, 'w').write(yaml.dump(config))
+
+if __name__ == '__main__':
+    import yaml
+    parser = build_arg_parser()
+    args = parser.parse_args()
+
+    if (not os.path.exists(default_config_dir)):
+        os.makedirs(default_config_dir)
+
+    if (args.action == 'upload'):
+        if (args.local_path is None):
+            parser.error('"upload" action requires local_path')
+        elif (not os.path.exists(args.local_path)):
+            parser.error("invalid local_path %s, the location doesn't exist" % args.local_path)
+
+    if (args.config is None):
+        config_file = default_config_dir + '/py1drive'
+    else:
+        config_file = args.config.name
+
+    if (args.key_store_dir is None):
+        key_store_dir = default_config_dir
+    else:
+        key_store_dir = args.key_store_dir
+
+    if (os.path.exists(config_file)):
+        config = yaml.load(open(config_file, 'r'))
+    else:
+        config = dict()
+    exec_action(args.action, args.local_path, args.remote_path)
