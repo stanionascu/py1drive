9ddd40a46162c3e7d0a3b45b2d58eedaa60744d9
Stanislav Ionascu <stanislav.ionascu@gmail.com>
2015-07-12 12:35:20 +0200

    fixes: wrong units displayed by common.readable_size

--- py1drive/common.py
+++ py1drive/common.py
@@ -6,17 +6,15 @@
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
 
-import os, math
+import os, math, time, sys
 
 def readable_size(size):
     if size == 0:
         return '0 B '
-    size_units = ('KB', 'MB', 'GB', 'TB')
-    unit = int(math.floor(math.log(size, 1024)))
-    if unit >= len(size_units):
-        unit = len(size_units) - 1
-    size = round(size / math.pow(1024, unit), 2)
-    return "%s %s" % (size, size_units[unit])
+    size_units = ('B', 'KB', 'MB', 'GB', 'TB')
+    unit = min(math.floor(math.log(size, 1024)), len(size_units) - 1)
+    size_fmt = round(size / math.pow(1024, unit), 2)
+    return "%s %s" % (size_fmt, size_units[unit])
 
 def arg_is_dir(parser, path):
     if (os.path.isdir(path)):
